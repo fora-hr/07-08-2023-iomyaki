17a133797c93e96902e89323218f6c423385370c
iomyaki <io.mknkv@gmail.com>
2023-08-13 15:48:21 +0700

    Create tableOfWinners.py
    This script builds a table of winners based on their run time

---
+++ tableOfWinners.py
@@ -0,0 +1,44 @@
+import json
+from datetime import datetime
+import pandas as pd
+from IPython.display import display
+
+competitorsPath = 'competitors2.json'
+with open(competitorsPath, 'r', encoding='utf-8-sig') as competitorsFile:
+    competitors = json.load(competitorsFile)
+
+resultsPath = 'results_RUN.txt'
+with open(resultsPath, 'r', encoding='utf-8-sig') as resultsFile:
+    results = resultsFile.read()
+
+resultsOrderNumber = sorted(results.split('\n'), key=lambda line: (int(line.split(' ')[0]), line.split(' ')[1]))
+
+resultsTime = []
+
+for i in range(0, len(resultsOrderNumber), 2):
+    resultsTime.append(
+        [
+            resultsOrderNumber[i].split(' ')[0],
+            datetime.strptime(resultsOrderNumber[i].split(' ')[2], '%H:%M:%S,%f') - datetime.strptime(
+                resultsOrderNumber[i + 1].split(' ')[2], '%H:%M:%S,%f')
+        ]
+    )
+
+resultsOrderTime = sorted(resultsTime, key=lambda res: res[1])
+
+df = pd.DataFrame(columns=['Занятое место', 'Нагрудный номер', 'Имя', 'Фамилия', 'Результат'])
+
+place = 1
+for res in resultsOrderTime:
+    df = df._append(
+        {
+            'Занятое место': place,
+            'Нагрудный номер': res[0],
+            'Имя': competitors[res[0]]['Name'],
+            'Фамилия': competitors[res[0]]['Surname'],
+            'Результат': (datetime.strptime(str(res[1]), '%H:%M:%S.%f')).strftime('%M:%S,%f')[:-4]
+        }, ignore_index=True
+    )
+    place += 1
+
+display(df.to_string(index=False))
